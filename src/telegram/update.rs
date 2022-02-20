bf92a2c652960495947560641c089770e6515fef
pan93412 <pan93412@gmail.com>
2022-02-21 00:00:38 +0800

    style: cargo fmt

--- src/telegram/update.rs
+++ src/telegram/update.rs
@@ -62,7 +62,7 @@ impl Handler<ClientModuleMessage> for ClientModuleExecutor {
             // Make a Arc smart pointer to the message
             // so we can share it with those modules.
             let message = Arc::new(RwLock::new(message?));
-            
+
             for module in modules.iter() {
                 // Clone some context that the following code will use.
                 let module = module.clone();
@@ -76,13 +76,10 @@ impl Handler<ClientModuleMessage> for ClientModuleExecutor {
                     // this to let the every modules consume.
                     let error = module
                         .recipient
-                        .send(ModuleMessage {
-                            handle,
-                            message,
-                        })
+                        .send(ModuleMessage { handle, message })
                         .await
                         .unwrap();
-    
+
                     // module.name is the module name;
                     // e is the error from module.recipient.send().
                     if let Err(e) = error {
